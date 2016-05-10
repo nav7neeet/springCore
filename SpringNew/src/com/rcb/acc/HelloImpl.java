50870d0293d830967771f34383d757ecce89e1df
slkdjflsd <test>
2016-05-10 10:53:02 +0530

    Constructor argument tested!!

--- SpringNew/src/com/rcb/acc/HelloImpl.java
+++ SpringNew/src/com/rcb/acc/HelloImpl.java
@@ -2,9 +2,15 @@ package com.rcb.acc;
 
 public class HelloImpl implements Hello
 {
+
+	public HelloImpl(int i, String s)
+	{
+		System.out.println("test const inj "+i+s);
+	}
+
 	@Override
 	public void fun(String s)
 	{
-		System.out.println("springing back to life "+s);
+		System.out.println("springing back to life -"+s);
 	}
 }
